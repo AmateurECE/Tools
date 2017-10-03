ca04c7b45f3a326da9522c0194f8da71031cef25
Ethan Twardy <etwardy@xes-inc.com>
2017-10-03 09:18:43 -0500

    Some new changes--UNSTABLE

---
+++ FlowDoc/hash.h
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * NAME:	    hash.h
+ *
+ * AUTHOR:	    Ethan D. Twardy
+ *
+ * DESCRIPTION:	    This file contains the public interface for the hashmap
+ *		    source code in hash.c. This hash is designed to work with
+ *		    the tree that is in tree.h, so it has a few caveats. It
+ *		    is an associative hash, meaning that the interface functions
+ *		    are passed a data point and the hashing function will return
+ *		    a pointer to that data. This is used in conjunction with
+ *		    the tree to prevent bloating of the tree. Because of this,
+ *		    this implementation is not very useful when it comes to data
+ *		    serialization.
+ *
+ * CREATED:	    10/02/2017
+ *
+ * LAST EDITED:	    10/02/2017
+ ***/
+
+#ifndef __ET_HASH_H__
+#define __ET_HASH_H__
+
+/*******************************************************************************
+ * TYPE DEFINTIONS
+ ***/
+
+typedef struct {
+  
+  size_t size;
+  size_t capacity;
+  int (*h1)(const void * key);
+  int (*h2)(const void * key);
+  void * (*compare)(const void * q, const void * r);
+  bucket_t ** buckets;
+
+} hash_t;
+
+typedef struct {
+
+  void * key; /* The key to compare the key given (Ensure data is correct) */
+  void * data; /* Data */
+  
+} bucket_t;
+
+/*******************************************************************************
+ * API FUNCTION PROTOTYPES
+ ***/
+
+hash_t * hash_create(size_t capacity);
+int hash_insert(hash_t * hash, void * key, void * data);
+void * hash_get(hash_t * hash, void * key);
+int hash_remove(hash_t * hash, void * key);
+void hash_destroy(hash_t * hash);
+
+#endif /* __ET_HASH_H__ */
+
+/******************************************************************************/
--- FlowDoc/tree.h
+++ FlowDoc/tree.h
@@ -4,7 +4,17 @@
  * AUTHOR:	    Ethan D. Twardy
  *
  * DESCRIPTION:	    This file contains the public interface for the source in
- *		    tree.c, which is an n-ary tree.
+ *		    tree.c, which is an n-ary tree. This tree is designed for
+ *		    the FlowDoc program, so it has a few caveats. For example,
+ *		    only one copy of each node may remain in memory at any time.
+ *		    This is done by using an associative hash map to keep track
+ *		    of all the data in the tree. This prevents bloating when
+ *		    analyzing larger products that may call strtoul() hundreds
+ *		    of times, for example. This tree is not a search tree. The
+ *		    nodes of the children are not ordered, however the general
+ *		    theme is to insert children to the right of any previously
+ *		    existing children, so that the tree may be generally viewed
+ *		    chronologically when traversed inorder.
  *
  * CREATED:	    09/29/2017
  *
@@ -35,14 +45,20 @@ typedef struct _nary_node {
 
 } nary_node_t;
 
+/* TODO: Maybe keep track of all of the nodes that reference the function? */
+
 /**
  * \brief N-Ary Tree Struct.
- * \member 
+ * \member root The root node.
+ * \member compare Function to compare two data points.
+ * \member destroy Function to destroy a data point.
  */
 typedef struct {
 
   _nary_node * root;
-  /* Other members here. */
+  void * (*compare)(const void * data1, const void * data2);
+  void (*destroy)(void * data);
+  /* TODO: Hash pointer here */
 
 } nary_tree_t;
 
@@ -50,7 +66,8 @@ typedef struct {
  * API FUNCTION PROTOTYPES
  ***/
 
-nary_tree_t * nary_tree_create(void);
+nary_tree_t * nary_tree_create(void * (*compare)(const void *, const void *),
+			       void (*destroy)(void * data));
 int nary_tree_insert(nary_tree_t * tree, nary_tree_t * node);
 int nary_tree_remove(nary_tree_t * tree, nary_tree_t * node);
 void nary_tree_destroy(nary_tree_t * tree);
