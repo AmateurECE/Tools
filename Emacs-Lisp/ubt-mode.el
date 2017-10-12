723b1610c68ea654fc469047586822797ee2de85
Ethan Twardy <etwardy@xes-inc.com>
2017-10-12 17:24:19 -0500

    Some style fixes and preprocessor directive additions This adds the preprocessor tokens "@if," "@ifdef," "@ifndef," "@else," and "@endif" to the fontification list.

--- Emacs-Lisp/ubt-mode.el
+++ Emacs-Lisp/ubt-mode.el
@@ -18,26 +18,26 @@
 
 ;; Add to this variable when new directives are defined.
 (defvar ubt-preprocessor-directives
-  '("include"))
+  '("include" "define" "if" "ifdef" "ifndef"))
 
 (defvar ubt-font-lock-preprocessor-directives
-  `(
-    ("^[[:blank:]]*\\(@\\<include\\>\\)[[:blank:]]*\\([[:graph:]]+\\)"
+  `(("^[[:blank:]]*\\(@\\<include\\>\\)[[:blank:]]*\\([[:graph:]]+\\)"
      (1 font-lock-function-name-face)
      (2 font-lock-variable-name-face nil t))
     (,(concat "\\<\\(printenv\\|sete\\(?:nv\\|xpr\\(?:\\.[blsw]\\)?\\)\\)\\>"
 	      "[[:blank:]]*\\([[:graph:]]+\\)")
      (2 font-lock-variable-name-face))
+    ("^[[:blank:]]*\\(@\\<define\\>\\)[[:blank:]]*\\([[:alnum:]_]+\\)"
+     (1 font-lock-function-name-face)
+     (2 font-lock-variable-name-face nil t))
+    (,(regexp-opt '("@if" "@ifdef" "@ifndef" "@else" "@endif") t)
+     . font-lock-function-name-face)
     ("\\(itest\\(?:\\.[blsw]\\)?\\)" . font-lock-preprocessor-face)
-    ("\\<run\\>" . font-lock-preprocessor-face)
-    )
-  )
+    ("\\<run\\>" . font-lock-preprocessor-face)))
 
 (define-derived-mode ubt-mode sh-mode "U-Boot Script"
   "This mode is for editing U-Boot script files."
-  
-  (font-lock-add-keywords nil ubt-font-lock-preprocessor-directives)
-)
+  (font-lock-add-keywords nil ubt-font-lock-preprocessor-directives))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; PROCEDURAL STATEMENTS
