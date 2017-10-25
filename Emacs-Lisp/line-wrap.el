3d2193b84b0ff1ac1796f29d3f50f43f459b124d
Ethan Twardy <etwardy@xes-inc.com>
2017-10-25 10:08:35 -0500

    It works now!
    This commit marks the stability of the line-wrap() module. Load it
    in your .emacs file to use its functions. line-wrap() truncates all
    lines which extend beyond the custom variable line-wrap-column-number,
    and line-wrap-test-lines() tests all the lines in the region to
    determine if they extend beyond the column number. In addition, there
    are a number of custom variables which may be changed to tweak the
    behavior of the functions to your liking.

--- Emacs-Lisp/line-wrap.el
+++ Emacs-Lisp/line-wrap.el
@@ -1,3 +1,4 @@
+;;; -*- lexical-binding: t -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; NAME:	    line-wrap.el
 ;;
@@ -24,6 +25,12 @@
   :type 'boolean
   :group 'line-wrap)
 
+(defcustom line-wrap-truncate-unbroken-strings t
+  "When non-nil, line-wrap() will truncate any lines that consist of long,
+unbroken character strings, for example, URLs."
+  :type 'boolean
+  :group 'line-wrap)
+
 (defcustom line-wrap-delimiter 'line-wrap-default-delimiter-hook
   "A function that determines which delimiter to use, if
 `line-wrap-delimit-next-line' is non-nil. This function should return a cell of
@@ -35,19 +42,11 @@ longer than the actual length of the string."
   :options '(line-wrap-default-delimiter-hook)
   :group 'line-wrap)
 
-(defcustom line-wrap-truncate-unbroken-strings t
-  "When non-nil, line-wrap() will truncate any lines that consist of long,
-unbroken character strings, for example, URLs."
-  :type 'boolean
+(defcustom line-wrap-indentation 'line-wrap-default-indentation-hook
+  "A function that determines how to indent truncated lines in the current mode.
+This function takes no arguments, and should indent the next line accordingly."
+  :options '(line-wrap-default-indentation-hook)
   :group 'line-wrap)
-
-(defvar line-wrap-debug t
-  "If non-nil, debugging messages will be printed for line-wrap functions.")
-
-(defun lw-debug (strmsg)
-  "Debug messaging for line-wrap functions."
-  (if line-wrap-debug
-      (message strmsg)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; FUNCTIONS
@@ -68,7 +67,8 @@ unbroken character strings, for example, URLs."
 ;; NOTES:	    none.
 ;;;
 (defun line-wrap ()
-  "Wrap the lines in the region"
+  "Wrap the lines in the region, so that no line extends past the column with
+number `line-wrap-column-number'."
   (interactive)
   (setq deactivate-mark t)
   (if (not (use-region-p))
@@ -94,53 +94,82 @@ unbroken character strings, for example, URLs."
       			 "length). Please see the documentation for line-wrap-"
       			 "delimiter for more informtation."))
 	      t))
-	;; Begin, and go until the end of the line extends past the region.
-      	(while (< eol reg-end)
-	  ;; Only manipulate the lines which extend past the column.
-      	  (if (> eol (+ bol line-wrap-column-number))
-	      (progn
-		(goto-char (+ bol line-wrap-column-number))
-		(lw-debug
-		 (format "Going to char %d" (+ bol line-wrap-column-number)))
-		(skip-chars-backward "^[:space:]")
-		(if (not (= (point) bol))
-		    (progn
-		      ;; If we need to delimit, do so now.
-		      (if line-wrap-delimit-next-line
-			  (save-excursion
-			    (skip-chars-backward " \t")
-			    (while (> (+ (point) length)
-				      line-wrap-column-number)
-			      (lw-debug "This is where the loop is.")
-			      (skip-chars-backward "^[:space:]")
-			      (skip-chars-backward " \t"))
-			    (insert " " string)))
-		      ;; Then truncate the line.
-		      (insert "\n")
-		      (indent-for-tab-command)
-		      (previous-line)
-		      (beginning-of-line)
-		      (delete-trailing-whitespace bol eol))
-		  ;; We may still want to cut the line, though.
-		  (if line-wrap-truncate-unbroken-strings
-		      (progn
-			(goto-char (+ bol line-wrap-column-number))
-			(insert "\n")
-			(indent-for-tab-command))))))
-	  (if line-wrap-truncate-unbroken-strings
-	      (next-line)
-	    (next-logical-line))
+	(goto-char reg-end)
+	(if (= (point) (point-max))
+	    (setq reg-end (point-max))
+	  (progn
+	    (setq reg-end (save-excursion (end-of-line) (point)))))
+	(goto-char reg-beg)
+      	(while (< (point) reg-end) ;; Begin
+      	  (line-wrap-helper bol eol string length)
 	  (setq bol (save-excursion (beginning-of-line) (point)))
-	  (lw-debug (format "bol: %d" bol))
-	  (setq eol (save-excursion (end-of-line) (point)))
-	  (lw-debug (format "eol: %d" eol))))))
+	  (setq eol (save-excursion (end-of-line) (point)))))))
   (deactivate-mark t))
 
-(defun line-wrap-default-delimiter-hook ()
-  "Returns a cons cell of the form (string . length) to determine how to delimit
-newlines placed into the buffer."
-  (?\ . 1))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FUNCTION:	    line-wrap-helper
+;;
+;; DESCRIPTION:	    This function is a helper for line-wrap().
+;;
+;; ARGUMENTS:	    bol: The beginning of the current line.
+;;		    eol: The end of the current line.
+;;		    string: The string with which to delimit, if necessary.
+;;		    length: The length of the string.
+;;
+;; RETURN:	    none.
+;;
+;; NOTES:	    none.
+;;;
+(defun line-wrap-helper (bol eol string length)
+  "This function is simply a helper for line-wrap, to make things a little
+prettier."
+  ;; Only manipulate the lines which extend past the column.
+  (if (> eol (+ bol line-wrap-column-number))
+      (progn
+	(goto-char (+ bol line-wrap-column-number))
+	(skip-chars-backward "^[:space:]")
+	(if (not (= (point) bol))
+	    (progn
+	      ;; If we need to delimit, do so now.
+	      (if line-wrap-delimit-next-line
+		  (save-excursion
+		    (skip-chars-backward " \t")
+		    (while (> (+ (point) length)
+			      line-wrap-column-number)
+		      (skip-chars-backward "^[:space:]")
+		      (skip-chars-backward " \t"))
+		    (insert " " string)))
+	      ;; Then truncate the line.
+	      (insert "\n")
+	      (if line-wrap-indentation
+		  (funcall line-wrap-indentation)
+		(indent-for-tab-command))
+	      (previous-line)
+	      (beginning-of-line)
+	      (delete-trailing-whitespace bol eol))
+	  ;; We may still want to cut the line, though.
+	  (if line-wrap-truncate-unbroken-strings
+	      (progn
+		(goto-char (+ bol line-wrap-column-number))
+		(insert "\n")
+		(if line-wrap-indentation
+		    (funcall line-wrap-indentation)
+		  (indent-for-tab-command))))))
+    (next-line)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FUNCTION:	    line-wrap-test-lines
+;;
+;; DESCRIPTION:	    This function traverses the region to test if any of the
+;;		    lines extend beyond the limit. If so, the function quits,
+;;		    deactivates the region, and prints a message.
+;;
+;; ARGUMENTS:	    none.
+;;
+;; RETURN:	    none.
+;;
+;; NOTES:	    Deactivates the region after executing.
+;;;
 (defun line-wrap-test-lines ()
   "Determines if there are any lines in the region which extend beyond
 `line-wrap-column-number', and stops on the first one found."
@@ -149,14 +178,62 @@ newlines placed into the buffer."
       (error "There is no valid region to operate on."))
   (let ((bor (region-beginning))
 	(eor (region-end))
-	bol eol)
+	eol)
     (goto-char bor)
-    (while (< (setq eol (save-excursion (end-of-line) (point))) eor)
-      (if (> eol
-	     (+ (save-excursion (beginning-of-line) (point))
-		line-wrap-column-number))
-	  (progn
-	    (goto-char eol)
-	    (deactivate-mark t))))))
+    (catch 'break
+      (while (< (setq eol (save-excursion (end-of-line) (point))) eor)
+	(if (> eol
+	       (+ (setq bol (save-excursion (beginning-of-line) (point)))
+		  line-wrap-column-number))
+	    (throw 'break t))
+	(next-line)))
+    (goto-char eol)
+    (deactivate-mark t)
+    (if (> eol (+ bol line-wrap-column-number))
+	(message (format "Current line length is %d; Limit is %d"
+			 (- eol bol) line-wrap-column-number)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FUNCTION:	    line-wrap-default-delimiter-hook
+;;
+;; DESCRIPTION:	    This function returns a cons cell containing a string and
+;;		    string length with which to delimit newlines that are
+;;		    inserted into the buffer by line-wrap(). It is the default
+;;		    value for line-wrap-delimiter, but this variable may be
+;;		    changed by the user.
+;;
+;; ARGUMENTS:	    none.
+;;
+;; RETURN:	    (<string> . <length>)
+;;
+;; NOTES:	    none.
+;;;
+(defun line-wrap-default-delimiter-hook ()
+  "Returns a cons cell of the form (string . length) to determine how to delimit
+newlines placed into the buffer."
+  (?\ . 1))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FUNCTION:	    line-wrap-default-indentation-hook
+;;
+;; DESCRIPTION:	    This function indents the line according to the major-mode.
+;;		    This is the default value for the hook line-wrap-indentation
+;;		    but it can be changed by the user.
+;;
+;; ARGUMENTS:	    none.
+;;
+;; RETURN:	    none.
+;;
+;; NOTES:	    Indents the line accordingly.
+;;;
+(defun line-wrap-default-indentation-hook ()
+  "Indents the line according to the major mode. The default value for
+`line-wrap-indentation'. See documentation for further details."
+  (cond
+   ((or (eq major-mode 'fundamental-mode)
+	(eq major-mode 'text-mode))
+    nil)
+   (t
+    (indent-for-tab-command))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
