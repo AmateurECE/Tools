f4f2867ed45e045c830fb7a508e843c08fdf30a9
Ethan Twardy <etwardy@xes-inc.com>
2017-11-28 16:57:59 -0600

    Initial commit for 'rewards'

---
+++ rewards/include/tasks.h
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * NAME:	    tasks.h
+ *
+ * AUTHOR:	    Ethan D. Twardy
+ *
+ * DESCRIPTION:	    This contains the header file, in case the source is to be
+ *		    used elsewhere.
+ *
+ * CREATED:	    11/28/2017
+ *
+ * LAST EDITED:	    11/28/2017
+ ***/
+
+/* TODO: tasks.h */
+
+/******************************************************************************/
---
+++ rewards/src/tasks.c
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * NAME:	    tasks.c
+ *
+ * AUTHOR:	    Ethan D. Twardy
+ *
+ * DESCRIPTION:	    This is a tool which rewards the user for doing things that
+ *		    they set out to do. A reward/incentive system.>
+ *
+ * CREATED:	    11/28/2017
+ *
+ * LAST EDITED:	    11/28/2017
+ ***/
+
+/*******************************************************************************
+ * INCLUDES
+ ***/
+
+#include "../C-Preprocessor/error.h"
+
+/*******************************************************************************
+ * MAIN
+ ***/
+
+int main(int argc, char * argv[])
+{
+  if (argc < 2) {
+    /* TODO: Interactive shell-mode */
+    StopIf(1, 1, "This has not yet been implemented.");
+  }
+
+  /* TODO: Implement tasks.c here */
+}
+
+/******************************************************************************/
