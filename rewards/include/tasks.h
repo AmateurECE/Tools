bee95a0ab847654be81bbbd52ebdbc4fbf181086
Ethan Twardy <etwardy@xes-inc.com>
2017-11-29 16:55:12 -0600

    Continue work on rewards/ UNSTABLE

--- rewards/include/tasks.h
+++ rewards/include/tasks.h
@@ -8,9 +8,26 @@
  *
  * CREATED:	    11/28/2017
  *
- * LAST EDITED:	    11/28/2017
+ * LAST EDITED:	    11/29/2017
  ***/
 
-/* TODO: tasks.h */
+/*******************************************************************************
+ * API FUNCTION PROTOTYPES
+ ***/
+
+/* Run an audit, to see how much you've earned! */
+int audit();
+
+/* List all of the registered expenses for the last week */
+int costs();
+
+/* Reset the data (as in at the start of the week). */
+int reset();
+
+/* Add a new total to the list of expenses */
+int spent();
+
+/* Print out the tasks that haven't been completed, or all of the tasks */
+int tasks(int aflag);
 
 /******************************************************************************/
--- rewards/src/tasks.c
+++ rewards/src/tasks.c
@@ -8,14 +8,54 @@
  *
  * CREATED:	    11/28/2017
  *
- * LAST EDITED:	    11/28/2017
+ * LAST EDITED:	    11/29/2017
  ***/
 
 /*******************************************************************************
  * INCLUDES
  ***/
 
+#include <stdio.h>
+
+#include "tasks.h"
 #include "../C-Preprocessor/error.h"
+
+/*******************************************************************************
+ * MACRO DEFINITIONS
+ ***/
+
+#if !defined(COSTS_FILE) || !defined(TASKS_FILE)
+#error COSTS_FILE and TASKS_FILE must be defined.
+#endif
+
+#define COSTS	0
+#define TASKS	1
+
+/*******************************************************************************
+ * TYPE DEFINITIONS
+ ***/
+
+typedef struct {
+  float * value;
+  char * description;
+} payme_data_t;
+
+/*******************************************************************************
+ * STATIC FUNCTION PROTOTYPES
+ ***/
+
+static payme_data_t * init_data(int data);
+static int save_data(payme_data_t *);
+
+/*******************************************************************************
+ * API FUNCTIONS
+ ***/
+
+int audit();
+int costs();
+int reset();
+int spent();
+int tasks(int aflag);
 
 /*******************************************************************************
  * MAIN
@@ -29,6 +69,65 @@ int main(int argc, char * argv[])
   }
 
   /* TODO: Implement tasks.c here */
+}
+
+/*******************************************************************************
+ * STATIC FUNCTIONS
+ ***/
+
+/*******************************************************************************
+ * FUNCTION:	    init_data
+ *
+ * DESCRIPTION:	    This function initializes an array of data structs from the
+ *		    location of the data.
+ *
+ * ARGUMENTS:	    data: (int) -- the type of data to read; (COSTS or TASKS).
+ *
+ * RETURN:	    payme_data_t * -- Pointer to the array of structs.
+ *
+ * NOTES:	    none.
+ ***/
+static payme_data_t * init_data(int data)
+{
+  FILE * fin = NULL;
+
+  switch(data) {
+  case COSTS:
+    if ((fin = fopen(COSTS_LOCATION, "r")) == NULL)
+      return NULL;
+    break;
+  case TASKS:
+    break;
+  default:
+  }
+
+  return NULL;
+}
+
+/*******************************************************************************
+ * FUNCTION:	    save_data
+ *
+ * DESCRIPTION:	    This function writes the data out its location. The type of
+ *		    data is specified.
+ *
+ * ARGUMENTS:	    data: (payme_data_t *) -- the data to save
+ *		    type: (int) -- the type of data (COSTS or TASKS).
+ *
+ * RETURN:	    int -- 0 on success, -1 otherwise.
+ *
+ * NOTES:	    none.
+ ***/
+static int save_data(payme_data_t * data, int type)
+{
+  switch(type) {
+  case COSTS:
+    break;
+  case TASKS:
+    break;
+  default:
+  }
+
+  return -1;
 }
 
 /******************************************************************************/
