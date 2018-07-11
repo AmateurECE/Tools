AmateurECE/Tools

commit a1317a8051e5723a55df8394de16cbf866a0242d

Add my email address to file banners
This commit adds my Uni email address after my full name in the
file banner at the top of the file. I thought it might be good
practice to have.

Emacs-Lisp/insert-banner.el
@@ -9,7 +9,7 @@
 ;;
 ;; CREATED:	    06/16/2017
 ;;
-;; LAST EDITED:	    04/26/2018
+;; LAST EDITED:	    07/06/2018
 ;;;
 
 ;; ====== NOTE: ======
@@ -113,6 +113,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.")
+
+(defvar my-name "Ethan D. Twardy <edtwardy@mtu.edu>")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Function Definitions
@@ -219,7 +221,7 @@ end of the current comment, or nil if point is not currently in a comment."
 	  eos c)
       (beginning-of-buffer)
       (setq eos (end-of-comment))
-      (when (re-search-forward "Ethan D. Twardy" eos 'keep-point)
+      (when (re-search-forward my-name eos 'keep-point)
 	(re-search-forward "LAST EDITED:" eos 'keep-point)
 	(forward-whitespace 1)
 	(setq c (char-after (point)))
@@ -275,7 +277,7 @@ end of the current comment, or nil if point is not currently in a comment."
 
   (insert "\n" nl)
   (insert-and-tab " NAME:" name "\n" nl "\n" nl)
-  (insert-and-tab " AUTHOR:" "Ethan D. Twardy\n" nl "\n" nl)
+  (insert-and-tab " AUTHOR:" my-name "\n" nl "\n" nl)
   (insert-and-tab " DESCRIPTION:")
   (save-excursion
     (insert "\n" nl "\n" nl)
@@ -323,7 +325,7 @@ end of the current comment, or nil if point is not currently in a comment."
 
   (insert "\n" nl)
   (insert-and-tab " NAME:" name "\n" nl "\n" nl)
-  (insert-and-tab " AUTHOR:" "Ethan D. Twardy\n" nl "\n" nl)
+  (insert-and-tab " AUTHOR:" my-name "\n" nl "\n" nl)
   (insert " DESCRIPTION:")
   (indent-to-column 20)
   (save-excursion
@@ -422,8 +424,7 @@ end of the current comment, or nil if point is not currently in a comment."
   (insert-and-tab "\n * NAME:" name)
   (insert-and-tab "\n *\n * DESCRIPTION:")
   (save-excursion
-    (insert-and-tab "\n *\n * @author" "Ethan D. Twardy "
-		    "<edtwardy@mtu.edu>")
+    (insert-and-tab "\n *\n * @author" my-name)
     (insert-and-tab "\n *\n * CREATED:" date)
     (insert-and-tab " *\n * LAST EDITED:" date " */")))
 
@@ -440,7 +441,6 @@ end of the current comment, or nil if point is not currently in a comment."
 ;;
 ;; NOTES:	    TODO: insert-file-banner doesn't work for C++
 ;;		    TODO: Create end-of-file marker
-;;		    TODO: Add Email address in <>.
 ;;;
 (defun insert-file-banner ()
   "Insert a banner at the top of a file"

bits/bits.c
@@ -3,11 +3,12 @@
  *
  * AUTHOR:	    Ethan D. Twardy
  *
- * DESCRIPTION:	    TODO: bits.c
+ * DESCRIPTION:	    This file implements a basic tool to convert numbers from
+ *		    one base to another.
  *
  * CREATED:	    05/11/2018
  *
- * LAST EDITED:	    05/13/2018
+ * LAST EDITED:	    07/05/2018
  ***/
 
 /******************************************************************************

bugs
@@ -1,6 +1,4 @@
 LaTeX/template.tex: Add the method for including a pdf | id:1d9e436487b5f9917ac49196412344f426a3c2eb
-Emacs-Lisp/insert-banner.el: Add Email address in <>. | id:5491c12537c22726cb232508cad284885703a1a4
-bits/bits.c: bits.c | id:57bf674601d7d13b5e8bda9982aebdbeabffebc1
 Emacs-Lisp/line-wrap.el: Line delimiting is broken | id:5f6118be79702529b1d4d232f695c47242758595
 LaTeX/template.tex: Add the method for including an image | id:6285c2fef775e6887ce1be37cad26f0bfaf52ed4
 Emacs-Lisp/insert-banner.el: Create end-of-file marker | id:adb6bdb4c67abf67c6163a59f921ce655f86dedb

commit 0321c79305e377378a79f68f331cf028e2c64933

Fix break caused by commit a1317a80...
In commit a1317a80(...) we introduced a bug that broke the
`update-last-edited-date' function. This path remedies that fix.

Emacs-Lisp/insert-banner.el
@@ -9,7 +9,7 @@
 ;;
 ;; CREATED:	    06/16/2017
 ;;
-;; LAST EDITED:	    07/06/2018
+;; LAST EDITED:	    07/10/2018
 ;;;
 
 ;; ====== NOTE: ======
@@ -221,7 +221,7 @@ end of the current comment, or nil if point is not currently in a comment."
 	  eos c)
       (beginning-of-buffer)
       (setq eos (end-of-comment))
-      (when (re-search-forward my-name eos 'keep-point)
+      (when (re-search-forward "Ethan D. Twardy" eos 'keep-point)
 	(re-search-forward "LAST EDITED:" eos 'keep-point)
 	(forward-whitespace 1)
 	(setq c (char-after (point)))