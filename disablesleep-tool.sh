de54181e9543d0e1e61d6a35b92d062c9fcd3b39
AmateurECE <edtwardy@mtu.edu>
2019-01-03 00:01:49 -0500

    Allow disablesleep-tool to be interrupted
    This patch adds resetting the power management settings as an EXIT
    action for bash, so regardless of whether the 'sleep 1800' line
    completes successfully or the process is interrupted or terminated,
    these lines will execute.

--- disablesleep-tool.sh
+++ disablesleep-tool.sh
@@ -9,7 +9,7 @@
 #
 # CREATED:	    12/30/2018
 #
-# LAST EDITED:	    12/30/2018
+# LAST EDITED:	    01/03/2019
 ###
 
 if [ `whoami` != "root" ]; then
@@ -17,10 +17,15 @@ if [ `whoami` != "root" ]; then
     exit
 fi
 
+resetParams() {
+    pmset -b sleep 60
+    pmset -b disablesleep 0
+}
+
+trap resetParams EXIT
+
 pmset -b sleep 0
 pmset -b disablesleep 1
 sleep 1800 # Process sleeps for 30 minutes
-pmset -b sleep 60
-pmset -b disablesleep 0
 
 ##############################################################################
