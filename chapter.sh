599cc493ab54e33507575d358e783936882981cf
AmateurECE <edtwardy@mtu.edu>
2019-07-16 19:14:00 -0500

    Add tools for manipulating mp3s

--- chapter.sh
+++ chapter.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/local/bin/bash
 ###############################################################################
 # NAME:             chapter.sh
 #
@@ -9,32 +9,85 @@
 #
 # CREATED:          05/30/2019
 #
-# LAST EDITED:      07/11/2019
+# LAST EDITED:      07/16/2019
 ###
+
+die()
+{
+    printf '%s\n' "$1"
+    exit 1
+}
+
+exitFn() {
+    rm -f "$tempFiles"
+    exit 0
+}
+
+trap exitFn INT
+trap exitFn EXIT
 
 inputFile="$1"
 outputFileBase="$2"
 
+if [[ "x$1" = "x" || "x$2" = "x" ]]; then
+    die "Usage: chapter.sh <input> <outputBase>"
+fi
+
 printf 'Input: %s\nOutput: %s\n\n' "$inputFile" "$outputFileBase"
+
+# Set up the log file
+logFile="./chapterScript.log"
+errorMessage="Error: ffmpeg failed. See the log for more information"
+tempFiles=""
+
+# First, get the album artwork
+artworkFile=$(mktemp -q /tmp/$(basename $0).XXXXXX)
+tempFiles="${tempFiles}${IFS}${artworkFile}"
+
+printf '%s\n' "Obtaining artwork from source file..."
+copyArtwork="yes"
+ffmpeg -i "$inputFile" "${artworkFile}.jpg" > "$logFile" 2>&1
+if [[ $? != "0" ]]; then
+    printf '%s\n' "Unable to obtain artwork. No artwork will be copied."
+    copyArtwork="no"
+fi
+
+inputExt=$(echo $1 | awk -F'.' '{print $NF}')
+intermediateFile=$(mktemp -q /tmp/$(basename $0).XXXXXX.${inputExt})
+tempFiles="${tempFiles}${IFS}${intermediateFile}"
 
 IFS=$'\n'
 chapters=`ffmpeg -i "$inputFile" 2>&1 | grep 'Chapter #'`
+lastChapter=$(echo "$chapters" | awk 'BEGIN{RS="";FS="\n"}{print $NF}' \
+                  | awk -F':' '{print $2}')
 for chapter in $chapters; do
     start=`echo $chapter | awk '{print $4}' | sed -e 's/,//'`
     end=`echo $chapter | awk '{print $6}'`
-    number=`echo $chapter | awk -F':' '{print $2}'`
+    number=$(printf '%02d' `echo $chapter | awk -F':' '{print $2}'`)
 
     # Get duration
     duration=`python -c "print($end - $start)"`
 
-    printf 'Chapter %s:' "$number"
+    printf 'Chapter %s/%s:\n' "$number" "$lastChapter"
     printf 'Start: %s; End: %s; Duration: %s\n' "$start" "$end" "$duration"
-    outputFile="${outputFileBase}-ch${number}.mp3"
+    outputFile="${outputFileBase}Chapter-${number}.mp3"
     printf 'OutputFile: %s\n' "$outputFile"
 
     # Get the chapter
-    ffmpeg -ss "$start" -t "$duration" -i "$inputFile" "$outputFile"
+    printf '%s\n' "Copying chapter to file..."
+    ffmpeg -y -ss "$start" -t "$duration" -vsync 2 -i "$inputFile" \
+           "$outputFile" > "$logFile" 2>&1 || die "$errorMessage"
+
+    # Do the album artwork (maybe)
+    if [[ $copyArtwork = "yes" ]]; then
+        printf '%s\n' "Fixing up artwork..."
+        ffmpeg -y -i "$outputFile" -i "${artworkFile}.jpg" -map 0:0 -map 1:0 \
+               -c copy -id3v2_version 3 -metadata:s:v title="Album cover" \
+               -metadata:s:v comment="Cover (front)" \
+               "${intermediateFile}.${inputExt}" \
+               > "$logFile" 2>&1 || die "$errorMessage"
+        cp -f "${intermediateFile}.${inputExt}" "$outputFile"
+    fi
 done
 
 ###############################################################################
-
---
+++ retag.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+###############################################################################
+# NAME:             retag.sh
+#
+# AUTHOR:           Ethan D. Twardy <edtwardy@mtu.edu>
+#
+# DESCRIPTION:      Use this script to rename and retag audio files in the
+#                   current directory.
+#
+# CREATED:          07/16/2019
+#
+# LAST EDITED:      07/16/2019
+###
+
+usage()
+{
+    printf '%s\n' \
+           "retag.sh <directory path>" \
+           "" \
+           "This script will use the program AudioQueueOutput in Git/Apple/"\
+           "to assist you in renaming and retagging audiobook files. Invoke"\
+           "using the path of a directory containing audiobook files and"\
+           "simply follow the prompts."
+}
+
+if [[ "x$1" = "x" || "$1" = "-h" || "$1" = "--help" ]]; then
+    usage
+fi
+
+play="$HOME/Git/Apple/CoreAudioExperiments/AudioQueueOutput/build/Release/\
+AudioQueueOutput"
+
+# Currently, the script only supports mp3 files. Also, it's closely coupled to
+# the naming convention used by ~/Git/Tools/chapter.sh.
+IFS=$'\n'
+numberChapters=$(ls *.mp3 | sort | awk 'BEGIN{RS="";FS="\n"}{print $NF}' \
+                     | sed -e 's/Chapter-\([0-9][0-9]\)\.mp3/\1/')
+numberChapters=$(expr "$numberChapters" + 1)
+for f in `ls *.mp3`; do
+    chapter=$(echo $f | sed -e 's/Chapter-\([0-9][0-9]\)\.mp3/\1/')
+    chapter=$(printf '%02g' $(expr $chapter + 1))
+    printf 'Chapter: %s\n' "$chapter"
+
+    trap "printf '%s' 'Title: '" INT
+    eval "($play $f)"
+    trap - INT
+    read title
+    if [[ "x$title" = "x" ]]; then
+        title="Chapter ${chapter}"
+    fi
+
+    mid3v2 --TIT2 "$title" --TRCK "${chapter}/${numberChapters}" "$f"
+    mid3v2 --delete-frames=CHAP "$f"
+    mv "$f" "${chapter} ${title//[^A-Za-z0-9 ._-]/_}.mp3"
+done
+
+###############################################################################
